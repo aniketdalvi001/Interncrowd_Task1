6c01ddd80d22ced90935dca6c02319b797c6cc39
aniketdalvi001 <124582255+aniketdalvi001@users.noreply.github.com>
2023-02-21 20:54:49 +0530

    Add files via upload

---
+++ INTERNCR.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ clrscr();
+ printf("List Of Birthday's\n");
+ printf("\n1].");
+ printf("Name: Abhijith Radhakrisnan \n");
+ printf("Dob: 29 May 2002 \n");
+ printf("Email:abhiradhakrishan29@gmail.com \n\n");
+ printf("2].");
+ printf("Name: Shubham Karpe \n");
+ printf("Dob: 23 January 2001 \n");
+ printf("Email:shubhamkarpe01@gmail.com \n\n");
+ printf("3].");
+ printf("Name: Karan Aushikar \n");
+ printf("Dob: 24 March 2003 \n");
+ printf("Email:karanaushikar12@gmail.com \n\n");
+ printf("4].");
+ printf("Name: Vivek Andhale \n");
+ printf("Dob: 12 August 1999 \n");
+ printf("Email:vivekandhale30@gmail.com \n\n");
+ printf("5].");
+ printf("Name: Rushikesh Bothe \n");
+ printf("Dob: 19 December 2000 \n");
+ printf("Email:rushibothe2@gmail.com \n\n");
+ getch();
+}
+
